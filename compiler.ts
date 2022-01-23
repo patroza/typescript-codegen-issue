e2cb2d2db307ec2353b3d4f06ac2f27e88007009
Patrick Roza <contact@patrickroza.com>
2022-01-23 21:39:07 +0100

    alt test

--- compiler.ts
+++ compiler.ts
@@ -2,44 +2,22 @@ import ts from "typescript"
 
 export function processNode(tc: ts.TypeChecker) {
   return (n: ts.Node) => {
-    if (/*ts.isClassDeclaration(n) || ts.isTypeAliasDeclaration(n)*/ true) {
-      const modelName = (n as any).name?.escapedText
+    const t = tc.getTypeAtLocation(n)
 
-      if (!modelName?.endsWith("Constructor")) {
-        return
-      }
-
-      const t = tc.getTypeAtLocation(n)
-
-      const result = { encoded: "", parsed: "" }
-      t.getProperties().forEach((c) => {
-        const method = c.name
-        if (method === "encoded" || method === "parsed") {
-          const tt = tc.getTypeOfSymbolAtLocation(c, n)
-          const typeDecl = tc.typeToString(
-            tt,
-            n,
-            ts.TypeFormatFlags.NoTruncation
-            //ts.TypeFormatFlags.None
-            //ts.TypeFormatFlags.AddUndefined |
-            // | ts.TypeFormatFlags.NoTypeReduction
-            //    | ts.TypeFormatFlags.MultilineObjectLiterals
-            //   | ts.TypeFormatFlags.InTypeAlias
-              | ts.TypeFormatFlags.UseAliasDefinedOutsideCurrentScope // prevents import(*)
-            //  | ts.TypeFormatFlags.UseStructuralFallback
-          )
-          const str = typeDecl
-          result[method] = str
-        }
-      })
-
-      if (!result.parsed) {
-        throw new Error("No parsed result")
-      }
-
-      return [
-        `export interface ${modelName.replace("Constructor", "")} ${result.parsed}`
-      ]
-    }
+    const typeDecl = tc.typeToString(
+      t,
+      n,
+      ts.TypeFormatFlags.NoTruncation
+      //ts.TypeFormatFlags.None
+      //ts.TypeFormatFlags.AddUndefined |
+      // | ts.TypeFormatFlags.NoTypeReduction
+      //    | ts.TypeFormatFlags.MultilineObjectLiterals
+         | ts.TypeFormatFlags.InTypeAlias
+        //| ts.TypeFormatFlags.UseAliasDefinedOutsideCurrentScope // prevents import(*)
+      //  | ts.TypeFormatFlags.UseStructuralFallback
+    )
+    return [
+      `export interface Test ${typeDecl}`
+    ]
   }
 }
--- test.ts
+++ test.ts
@@ -1,11 +1,7 @@
-export class Test {
-  static parsed: {
-    failedTest: string | null
-    failedTest2: string | undefined
-    succeededTest: string | number
-    succeedTest2: null
-    succeedTest3: undefined
-  }
+export type TestConstructor = {
+  failedTest: string | null
+  failedTest2: string | undefined
+  succeededTest: string | number
+  succeedTest2: null
+  succeedTest3: undefined
 }
-
-export type TestConstructor = typeof Test
